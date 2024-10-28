91630220e9333e30afa53d1209b2636a3b04e0cb
unknown <ahsansagor449@gmail.com>
2024-10-28 08:31:30 +0600

    register update

--- register.php
+++ register.php
@@ -5,34 +5,35 @@ require('connection.php');
 <html lang="en">
 
 <head>
-    <title>Login</title>
+    <title>Register</title>
 </head>
 
 <body>
     <?php
     if (isset($_POST['submit'])) {
+        $users_name     = $_POST['users_name'];
         $users_email    = $_POST['users_email'];
         $users_password = $_POST['users_password'];
 
-        $sql = "SELECT * FROM `users` WHERE email='$users_email' AND password='$users_password'";
+        $sql = "INSERT into `users` (username,email,password) values ('$users_name','$users_email','$users_password')";
 
         $result = mysqli_query($conn, $sql);
 
-        echo $sql;
-
-        // if ($result) {
-        //     header('location:index.php');
-        // } else {
-        //     die(mysqli_error($conn));
-        // }
+        if ($result) {
+            header('location:login.php');
+        } else {
+            die(mysqli_error($conn));
+        }
     }
     ?>
     <form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
+       Name :<br>
+       <input type="text" name="users_name"><br><br>
         Email :<br>
         <input type="email" name="users_email"><br><br>
         Password :<br>
         <input type="password" name="users_password"><br><br>
-        <button type="submit" name="submit">Login</button>
+        <button type="submit" name="submit">Save</button>
     </form>
 </body>
 
